320db2712b6ee08ba2cb411379fccf98dc87c59e
dipak-trio-spire <dipak@trio-spire.com>
2025-04-24 17:04:14 +0530

    add new gsap

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,23 @@ const nextConfig = {
   images: {
     domains: ["adaptive.rocket-wp.com"],
   },
+  experimental: {
+    scrollRestoration: true,
+    turbopack: false,
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.alias["lodash"] = "lodash-es";
+    }
+
+    config.optimization.splitChunks = {
+      chunks: "all",
+      minSize: 20 * 1024,
+      maxSize: 200 * 1024,
+    };
+
+    return config;
+  },
 };
 
 export default nextConfig;
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,3 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "../../public/fonts/inter.css";
 import "../../public/fonts/Ivyfont.css";
 import "../../public/fonts/overpassfont.css";
@@ -6,16 +5,6 @@ import "./globals.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { TransitionProvider } from "../../utile/transition-provider";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "Adaptive",
